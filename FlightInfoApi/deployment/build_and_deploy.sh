5b07db0844ca5f79ce1894f37c6279046844db15
Joao <joao.msilva@ceiia.com>
2019-12-07 17:38:01 +0000

    Configured docker support

---
+++ FlightInfoApi/deployment/build_and_deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ ! -f ./flightinfoapi.jar ]
+then
+  echo "Installing maven"
+  cd /opt/
+  wget http://www-eu.apache.org/dist/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz
+  tar -xvzf apache-maven-3.0.5-bin.tar.gz
+  mv apache-maven-3.0.5 maven
+  export M2_HOME=/opt/maven
+  export PATH=${M2_HOME}/bin:${PATH}
+  cd ..
+  echo "Building..."
+  mvn clean install
+  echo "moving jar file to deployment "
+  cp target/flightinfoapi.jar /deployment
+  mv deployment/ ../
+  echo "Cleaning code sources..."
+  cd ..
+  rm -rf FlightInfoApi/
+  cd deployment
+fi
+
+echo "Deployment..."
+java -jar flightinfoapi.jar
